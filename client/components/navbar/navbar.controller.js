d2d7a366f7b5f22fa6b8fdb0e93d478f7fa625cf
crownwangguan <crown.wg@outlook.com>
2016-03-04 12:19:41 -0500

    finish notice thing

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,27 +1,43 @@
 'use strict';
 
+(function() {
+
 class NavbarController {
   //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'link': '/'
-  }];
-
-  isCollapsed = true;
+  
   //end-non-standard
 
-  constructor($location, Auth) {
+  constructor($location, $scope, $http, Auth) {
+    this.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+    this.isCollapsed = true;
     this.$location = $location;
+    this.Auth = Auth;
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-    this.notice = (Auth.getCurrentUser.choosen == true) ? 1 : 0;
+    this.getCurrentUser = Auth.getCurrentUser();
+    this.users = [];
+    $http.get('/api/users').then(response => {
+      this.users = response.data;
+      socket.syncUpdates('user', this.users);
+    });
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('user');
+    });
   }
 
   isActive(route) {
     return route === this.$location.path();
   }
+
+  isee(){
+    this.Auth.changeMessage(this.getCurrentUser._id, false);
+    this.$location.path('/');
+  }
 }
 
 angular.module('crowdSourcingApp')
   .controller('NavbarController', NavbarController);
+})();
